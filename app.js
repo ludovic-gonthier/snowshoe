e659eeef6d0de478ccb9f1d6cc3f8b75e00a068b
ludovic-gonthier <gonthier.ludovic@gmail.com>
2015-05-07 21:34:51 +0200

    feat(app): add basic app with configuration

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var engine = require('express-react-views');
+var http = require('http');
+var livereload = require('connect-livereload');
+var morgan = require('morgan');
+var session = require('express-session');
+
+var config = require('./config');
+
+var app = express();
+var server = http.createServer(app);
+
+/*
+ * Application configuration
+ */
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views/pages');
+app.engine('jsx', engine.createEngine());
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({ secret: 'snowshoe cat, wilson\'s mustache rocks' }));
+app.use(express.static('public'));
+
+app.use(livereload());
+
+/*
+ * Server part
+ */
+server.on('listening', function () {
+  var separator = new Array(81).join('=');
+
+  console.log(separator);
+  console.log('Server created at ' + server.address().address + ':' + server.address().port);
+  console.log(separator);
+});
+
+server.listen(
+  process.env.PORT || config.get('server:port'),
+  process.env.ADRESS || config.get('server:hostname')
+);
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var gls = require('gulp-live-server');
+var jslint = require('gulp-jslint');
+var uglify = require('gulp-uglify');
+
+gulp.task('lint', function () {
+  var files = [
+    '*.js',
+    'views/bridges/**.js'
+  ];
+
+  return gulp.src(files)
+    .pipe(jslint({
+      "es6": true,
+      "strict": true,
+      "node": true,
+
+      "indent": 2,
+      "trailing": true,
+      "white": false,
+      "maxerr": 50,
+
+      "debug": false,
+      "devel": false,
+
+      "newcap": true,
+      "noempty": true,
+      "nomen": true,
+      "unparam": true
+    }))
+    .on('error', function (error) {
+      console.error(String(error));
+    });
+});
+
+gulp.task('reactify', function () {
+  var start = Date.now();
+
+  gulp.src(['views/bridges/**.js'])
+    // .pipe(uglify())
+    .pipe(browserify({
+      transform: [ 'reactify' ]
+    }))
+    .pipe(gulp.dest('public/views'))
+    .on('end', function () {
+      console.log('Complete!', (Date.now() - start) / 1000 + 's');
+    });
+
+  // gulp.watch('views/{components,bridges}/**.{js,jsx}', ['reactify']);s
+});
+
+gulp.task('server', function () {
+  var server = gls.new('app.js');
+
+  server.start();
+
+  gulp.watch([
+    'app.js',
+    '{lib,config,controllers}/**.js',
+    'views{components,pages}/**.jsx'
+  ], server.start);
+});
+
+gulp.task('default', ['reactify', 'server']);
