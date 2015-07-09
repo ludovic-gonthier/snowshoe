6fdbe5b3d27dc242f829943b9d3d29a7254c7981
ludovic-gonthier <gonthier.ludovic@gmail.com>
2015-07-09 22:12:25 +0200

    refactor(dashboard): io socket handlers into functions

--- views/components/github/dashboard.jsx
+++ views/components/github/dashboard.jsx
@@ -12,20 +12,8 @@ module.exports = React.createClass({
       pulls: []
     }
   },
-  componentDidMount: function () {
-    var socket = io();
-
-    socket.emit('user', {
-      user: this.props.user,
-      accessToken: this.props.accessToken
-    });
-
-    if (this.props.repositories_url) {
-      socket.emit('pulls', this.props.repositories_url);
-    }
-
-    socket.on('pulls', function (pulls) {
-      var ids = _.map(this.state.pulls, 'id');
+  handlePulls: function (pulls) {
+    var ids = _.map(this.state.pulls, 'id');
 
       _.forEach(pulls, function (pull) {
         var index = ids.indexOf(pull.id);
@@ -38,40 +26,53 @@ module.exports = React.createClass({
       }.bind(this));
 
       this.setState({pulls: _.sortByOrder(this.state.pulls, 'updated_at', [true, false])});
-    }.bind(this));
+  },
+  handlePullsDelete: function (deleted) {
+    if (deleted.length) {
+      this.setState({pulls: _.filter(this.state.pulls, function (pull) {
+        return deleted.indexOf(pull.id) === -1;
+      })});
+    }
+  },
+  handlePullsIssues: function (issues) {
+    var urls = _.map(this.state.pulls, 'url');
 
-    socket.on('pulls:delete', function (deleted) {
-      if (deleted.length) {
-        this.setState({pulls: _.filter(this.state.pulls, function (pull) {
-          return deleted.indexOf(pull.id) === -1;
-        })});
-      }
-    }.bind(this));
-
-    socket.on('pulls:issues', function (issues) {
-      var urls = _.map(this.state.pulls, 'url');
-
-      _.forEach(issues, function (issue) {
-        var index = urls.indexOf(issue.pull_request.url);
-
-        if (index !== -1) {
-          this.state.pulls[index].labels = issue.labels;
-          this.state.pulls[index].comments = issue.comments;
-        }
-      }.bind(this));
-
-      this.setState({pulls: this.state.pulls});
-    }.bind(this));
-
-    socket.on('pulls:status', function (status) {
-      var index = _.map(this.state.pulls, 'id').indexOf(status.pull_request.id);
+    _.forEach(issues, function (issue) {
+      var index = urls.indexOf(issue.pull_request.url);
 
       if (index !== -1) {
-        this.state.pulls[index].last_status = status;
+        this.state.pulls[index].labels = issue.labels;
+        this.state.pulls[index].comments = issue.comments;
       }
-
-      this.setState({pulls: this.state.pulls});
     }.bind(this));
+
+    this.setState({pulls: this.state.pulls});
+  },
+  handlePullsStatus: function (status) {
+    var index = _.map(this.state.pulls, 'id').indexOf(status.pull_request.id);
+
+    if (index !== -1) {
+      this.state.pulls[index].last_status = status;
+    }
+
+    this.setState({pulls: this.state.pulls});
+  },
+  componentDidMount: function () {
+    var socket = io();
+
+    socket.emit('user', {
+      user: this.props.user,
+      accessToken: this.props.accessToken
+    });
+
+    if (this.props.repositories_url) {
+      socket.emit('pulls', this.props.repositories_url);
+    }
+
+    socket.on('pulls', this.handlePulls);
+    socket.on('pulls:delete', this.handlePullsDelete);
+    socket.on('pulls:issues', this.handlePullsIssues);
+    socket.on('pulls:status', this.handlePullsStatus);
   },
   render: function () {
     return (
