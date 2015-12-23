c1d90850f5e35c581659c7b524d19fbb8cfde5ee
ludovic-gonthier <gonthier.ludovic@gmail.com>
2015-12-23 22:14:57 +0100

    style: remove debug console

--- views/bridges/login.js
+++ views/bridges/login.js
@@ -4,5 +4,4 @@ var React = require('react');
 
 var LoginJumbotron = React.createFactory(require('../components/login-jumbotron.jsx'));
 
-console.log(document.getElementById('mount-login-jumbotron'));
 React.render(new LoginJumbotron(), document.getElementById("mount-login-jumbotron"));
